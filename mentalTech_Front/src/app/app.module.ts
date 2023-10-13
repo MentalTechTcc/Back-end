c4bded9cf91a8ccc0ad9f089bd2e73d9d5ab2107
Lorrany <lolo.souza80@yahoo.com>
2023-10-12 21:01:07 -0300

    Adiciona página inicial.
    Co-Authored-By: Bianca Sofia <sofiabrasil44@gmail.com>

--- mentalTech_Front/src/app/app.module.ts
+++ mentalTech_Front/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
-import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { CabecalhoComponent } from './views/cabecalho/cabecalho.component';
+import { RodapeComponent } from './views/rodape/rodape.component';
+import { PrincipalComponent } from './views/principal/principal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabecalhoComponent,
-    RodapeComponent
+    RodapeComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule,

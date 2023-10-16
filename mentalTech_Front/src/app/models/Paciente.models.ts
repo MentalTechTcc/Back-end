434621ab68be96b146b1daf0e690971f5219044c
Lorrany <lolo.souza80@yahoo.com>
2023-10-16 13:29:39 -0300

    Adiciona model paciente.

---
+++ mentalTech_Front/src/app/models/Paciente.models.ts
@@ -0,0 +1,10 @@
+export interface Paciente{
+  nome: string;
+  senha: string;
+  dataNascimento: Date;
+  telefone: string;
+  email: string;
+  administrador: boolean;
+  sexo: number;
+  dataCadastro: Date;
+}

be5899341664d604f104df9f5ef234e279af99b5
biancasofia <sofiabrasil44@gmail.com>
2023-10-25 23:36:41 -0300

    add inicio da lógica para atualizar dados nos mesmos campos de visualização

--- mentalTech_Front/src/app/views/profissional/meus-dados/meus-dados.component-profissional.ts
+++ mentalTech_Front/src/app/views/profissional/meus-dados/meus-dados.component-profissional.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Profissional } from 'src/app/models/Profissional.models';
+import { Profissional, ProfissionalRequestId } from 'src/app/models/Profissional.models';
 import { LoginUsuarioService } from 'src/app/services/login-usuario.service';
+import { AtualizacaoDadosProfisionalService } from 'src/app/services/atualizacao-dados-profisional.service';
 
 @Component({
   selector: 'app-meus-dados-profissional',
@@ -8,32 +9,67 @@ import { LoginUsuarioService } from 'src/app/services/login-usuario.service';
   styleUrls: ['./meus-dados-profissional.component.css']
 })
 export class MeusDadosProfissionalComponent implements OnInit {
-
   nome: string = '';
   telefone: string = '';
   emailProfissional: string = '';
-  senhaProfissional: string = '';
-  codigoProfissional: string= '';
   dataNascimentoProfissional: Date = new Date();
+  codigoProfissional: string = '';
   Profissional: any;
-
-  constructor(private loginService: LoginUsuarioService) { }
+  ProfissionalRequestId: any;
+  idPessoa:number=0;
+  
+  constructor(
+    private loginService: LoginUsuarioService,
+    private atualizacaoService: AtualizacaoDadosProfisionalService
+  ) {}
 
   ngOnInit(): void {
     this.loginService.getPerfilProfissional().subscribe(
       (data: Profissional) => {
         this.Profissional = data;
-
         this.nome = this.Profissional.nome;
         this.telefone = this.Profissional.telefone;
         this.emailProfissional = this.Profissional.email;
-        this.codigoProfissional= this.Profissional.codigoProfissional;
+        this.codigoProfissional = this.Profissional.codigoProfissional;
         this.dataNascimentoProfissional = this.Profissional.dataNascimento;
+        this.idPessoa= this.Profissional.idPessoa
+
+
+        this.ProfissionalRequestId = {
+          cpf: this.Profissional.cpf,
+          senha: this.Profissional.senha,
+          administrador: this.Profissional.administrador,
+          sexo: this.Profissional.sexo,
+          descricaoProfissional: this.Profissional.descricaoProfissional
+        };
       },
-      error => {
+      (error) => {
         console.error('Erro ao buscar especialistas:', error);
       }
     );
   }
 
+  atualizarProfissional() {
+
+    const dadosFormulario = {
+      nome: this.nome,
+      telefone: this.telefone,
+      email: this.emailProfissional,
+      dataNascimento: this.dataNascimentoProfissional,
+      codigoProfissional: this.codigoProfissional,
+      idPessoa: this.idPessoa,
+    };
+
+
+    const dadosAtualizados = { ...this.ProfissionalRequestId, ...dadosFormulario };
+
+    this.atualizacaoService.atualizarProfissional(dadosAtualizados).subscribe(
+      (response) => {
+        console.log('Dados atualizados com sucesso:', response);
+      },
+      (error) => {
+        console.error('Erro na atualização de dados:', error);
+      }
+    );
+  }
 }

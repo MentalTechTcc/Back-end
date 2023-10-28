e9baaeac659b0b7faee36d3ae5403face50c64cf
biancasofia <sofiabrasil44@gmail.com>
2023-10-28 14:33:26 -0300

    add configurações e funções para abrir a agenda para o paciente

--- mentalTech_Front/src/app/views/paciente/listar-profissionais/listar-profissionais.component.ts
+++ mentalTech_Front/src/app/views/paciente/listar-profissionais/listar-profissionais.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Profissional } from 'src/app/models/Profissional.models';
 import { CadastroProfissionalService } from 'src/app/services/cadastro-profissional.service';
+import { CadastroAgendaProfissionalService } from 'src/app/services/cadastro-agenda-profissional.service';
+import { Agenda } from 'src/app/models/Agenda.models';
 
 @Component({
   selector: 'app-listar-profissionais',
@@ -10,18 +12,33 @@ import { CadastroProfissionalService } from 'src/app/services/cadastro-profissio
 export class ListarProfissionaisComponent implements OnInit {
 
   especialistas: Profissional[] = [];
+  agendaDoProfissional: Agenda[] = [];
 
-  constructor(private service: CadastroProfissionalService) {}
+  constructor(
+    private service: CadastroProfissionalService,
+    private agendaService: CadastroAgendaProfissionalService
+  ) {}
 
   ngOnInit(): void {
     this.service.listar().subscribe(
       (data: Profissional[]) => {
         this.especialistas = data;
       },
-      error => {
+      (error) => {
         console.error('Erro ao buscar especialistas:', error);
       }
     );
   }
 
+  exibirAgenda(cpfProfissional: string): void {
+    this.agendaService.listarPorCpf(cpfProfissional).subscribe(
+      (data: Agenda[]) => {
+        this.agendaDoProfissional = data;
+        console.log('Agenda do Profissional:', this.agendaDoProfissional);
+      },
+      (error) => {
+        console.error('Erro ao buscar a agenda do profissional:', error);
+      }
+    );
+  }
 }

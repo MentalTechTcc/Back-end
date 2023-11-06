e7ad17e2a874ccad2d8d7ec12475581708a1a973
biancasofia <sofiabrasil44@gmail.com>
2023-11-05 21:09:31 -0300

    add teste do getAll

---
+++ mentalTech_Python/src/tests/application/controllers/test_pessoaController.py
@@ -0,0 +1,23 @@
+import pytest
+from httpx import AsyncClient
+from fastapi import status
+
+
+
+GLOBAL_RESPONSE = []
+HTTPS_PESSOA = "http://localhost:9091"
+
+
+
+# GET ALL
+@pytest.mark.asyncio
+async def testReadAll():
+    async with AsyncClient(base_url=HTTPS_PESSOA, timeout=30.0) as async_client:
+        response = await async_client.get("/pessoa/")
+    assert response.status_code == status.HTTP_200_OK
+
+
+
+
+
+

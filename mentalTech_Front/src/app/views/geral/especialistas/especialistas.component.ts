fb19c1f7082b30ba0d0afe9183ec812b6ba7c59d
biancasofia <sofiabrasil44@gmail.com>
2023-11-07 13:45:32 -0300

    add nova página que lista os profissionais, ajustando página de dentro da aplicação ...

--- mentalTech_Front/src/app/views/geral/especialistas/especialistas.component.ts
+++ mentalTech_Front/src/app/views/geral/especialistas/especialistas.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
 import { CadastroProfissionalService } from 'src/app/services/cadastro-profissional.service';
 import { Profissional } from 'src/app/models/Profissional.models';
+import { Especialidade } from 'src/app/models/Especialidade.models';
+import { Tematica } from 'src/app/models/Tematica.models';
+import { EspecialidadeServiceService } from 'src/app/services/especialidade-service.service';
+import { TematicaServiceService } from 'src/app/services/tematica-service.service';
 
 @Component({
   selector: 'app-especialistas',
@@ -11,23 +13,51 @@ import { Profissional } from 'src/app/models/Profissional.models';
 })
 export class EspecialistasComponent implements OnInit {
   especialistas: Profissional[] = [];
+  especialidadesPorProfissional: { [cpfProfissional: string]: string[] } = {}; //objeto
+  tematicasPorProfissional: { [cpfProfissional: string]: string[] } = {};  //objeto
 
-  constructor(private http: HttpClient, private service: CadastroProfissionalService) {}
+  constructor(
+    private service: CadastroProfissionalService,
+    private especialidadeService: EspecialidadeServiceService,
+    private tematicasService: TematicaServiceService
+    ) {}
 
-  ngOnInit(): void {
-    // Fazer uma solicitação HTTP para buscar os dados dos especialistas
-    // this.http.get<any[]>(`${environment.baseUrl}/profissional`)
-    // .subscribe((data: any[]) => {
-    //   this.especialistas = data;
-    // });
+    ngOnInit(): void {
+      this.service.listar().subscribe(
+        (data: Profissional[]) => {
+          this.especialistas = data;
+          this.obterEspecialidadesPorProfissional();
+          this.obterTematicasPorProfissional();
+        },
+        (error) => {
+          console.error('Erro ao buscar especialistas:', error);
+        }
+      );
+    }
 
-    this.service.listar().subscribe(
-      (data: Profissional[]) => {
-        this.especialistas = data;
-      },
-      error => {
-        console.error('Erro ao buscar especialistas:', error);
-      }
-    );
-  }
+    obterEspecialidadesPorProfissional(): void {
+      this.especialistas.forEach((especialista) => {
+        this.especialidadeService.listarEspecialidadesProfissional(especialista.cpf).subscribe(
+          (especialidades: Especialidade[]) => {
+            this.especialidadesPorProfissional[especialista.cpf] = especialidades.map(especialidade => especialidade.descricaoEspecialidade);
+          },
+          (error) => {
+            console.error('Erro ao buscar especialidades do profissional:', error);
+          }
+        );
+      });
+    }
+    
+    obterTematicasPorProfissional(): void {
+      this.especialistas.forEach((especialista) => {
+        this.tematicasService.listarTematicasProfissional(especialista.cpf).subscribe(
+          (tematicas: Tematica[]) => {
+            this.tematicasPorProfissional[especialista.cpf] = tematicas.map(tematica => tematica.nomeTematica);
+          },
+          (error) => {
+            console.error('Erro ao buscar tematicas do profissional:', error);
+          }
+        );
+      });
+    }
 }
